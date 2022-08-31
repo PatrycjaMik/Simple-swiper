cc1719545ea1f395e1c6332923d2de8fc4c19b90
PatrycjaMik <80001646+PatrycjaMik@users.noreply.github.com>
2022-08-31 16:39:17 +0200

    simple swiper for four slides

---
+++ index.js
@@ -0,0 +1,11 @@
+
+  window.onload = function() {
+    let swiper = new Swiper('.slider-host', {
+     pagination: {
+    el: '.swiper-dots',
+    clickable: true,
+  },
+   slidesPerView: 1,
+    });
+  }
+  
